48a2656ed40dab46abd6bbd6ca5e8c06e40d22c2
MikeDark <mkfist2911@gmail.com>
2022-12-08 15:37:37 +0300

    вариант2

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,24 +1,16 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
+﻿ string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            M = stroka.Split(' ');
+            var result = new string[M .Length];
+            var realSize = 0;
+            foreach (var value in M )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
